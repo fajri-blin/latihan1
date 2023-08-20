8eeebf8373e46a6b6a0a10cfd8ca81aa25ce5e84
fajri-blin <fajriindonesia1945@gmail.com>
2023-08-20 20:19:10 +0700

    fixing username and password

---
+++ app/src/main/java/com/example/latihan1/Home.kt
@@ -0,0 +1,43 @@
+package com.example.latihan1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.latihan1.ui.theme.Latihan1Theme
+
+class Home : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Latihan1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(){
+    Text(text = "This is Home Screen")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    Latihan1Theme {
+        HomeScreen()
+    }
+}
--- app/src/main/java/com/example/latihan1/MainActivity.kt
+++ app/src/main/java/com/example/latihan1/MainActivity.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,16 +20,29 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.latihan1.ui.theme.Latihan1Theme
+
+sealed class Screen(val route: String) {
+    object Login : Screen("login")
+    object Home : Screen("home")
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,29 +54,65 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginPage()
+                    AppNavigation() // Use AppNavigation here
                 }
             }
         }
     }
 }
 
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginPage(modifier: Modifier = Modifier){
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.Login.route) {
+        composable(Screen.Login.route) {
+            LoginPage(navController = navController) // Pass the NavController
+        }
+        composable(Screen.Home.route){
+            HomeScreen()
+        }
+    }
+}
+
+@Composable
+fun TopBar() {
+    Box(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(top = 0.dp, start = 0.dp, end = 0.dp) // Adjust the padding as needed
+    ) {
+        Text(
+            text = "Login",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            textAlign = TextAlign.Start,
+            color = MaterialTheme.colorScheme.onPrimary // Ensure text color is readable on the background color
+        )
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginPage(navController: NavController, modifier: Modifier = Modifier) {
     val imageResource = painterResource(id = R.drawable.ic_launcher_foreground)
+
+    // State variables to store the user input
+    val usernameState = remember { mutableStateOf("") }
+    val passwordState = remember { mutableStateOf("") }
 
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.padding(16.dp)
     ) {
-        Text(
-            text = "Login",
-            fontSize = 50.sp,
-            lineHeight = 50.sp,
-            textAlign = TextAlign.Center
-        )
+        TopBar()
         Image(
             painter = imageResource,
             contentDescription = null, // Provide a meaningful description
@@ -73,8 +123,8 @@ fun LoginPage(modifier: Modifier = Modifier){
         )
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = usernameState.value,
+            onValueChange = { usernameState.value = it },
             label = { Text(text = "Username") },
             singleLine = true,
             modifier = Modifier
@@ -83,8 +133,8 @@ fun LoginPage(modifier: Modifier = Modifier){
         )
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = passwordState.value,
+            onValueChange = { passwordState.value = it },
             label = { Text(text = "Password") },
             singleLine = true,
             visualTransformation = PasswordVisualTransformation(),
@@ -94,7 +144,7 @@ fun LoginPage(modifier: Modifier = Modifier){
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(
-            onClick = { /* Handle login button click here */ },
+            onClick = { navController.navigate(Screen.Home.route) },
             modifier = Modifier.fillMaxWidth()
         ) {
             Text(text = "Login")
@@ -106,6 +156,6 @@ fun LoginPage(modifier: Modifier = Modifier){
 @Composable
 fun GreetingPreview() {
     Latihan1Theme {
-        LoginPage()
+        LoginPage(navController = rememberNavController())
     }
 }
