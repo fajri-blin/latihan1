0a8e4f8b03a6aa6c2aa55b4b0c774824137b3ee2
fajri-blin <fajriindonesia1945@gmail.com>
2023-08-22 12:41:33 +0700

    adding toggle for password visibility

--- app/src/main/java/com/example/latihan1/LoginActivity.kt
+++ app/src/main/java/com/example/latihan1/LoginActivity.kt
@@ -11,8 +11,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -25,6 +30,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -57,6 +63,9 @@ fun LoginPage(navController: NavController, modifier: Modifier = Modifier) {
     // State variables to store the user input
     val usernameState = remember { mutableStateOf("") }
     val passwordState = remember { mutableStateOf("") }
+
+    // Creating a variable to store toggle state
+    var passwordVisible = remember { mutableStateOf(false) }
     TopBar("Login")
     Column(
         verticalArrangement = Arrangement.Center,
@@ -87,7 +96,20 @@ fun LoginPage(navController: NavController, modifier: Modifier = Modifier) {
             onValueChange = { passwordState.value = it },
             label = { Text(text = "Password") },
             singleLine = true,
-            visualTransformation = PasswordVisualTransformation(),
+            visualTransformation = if (passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                val image = if (passwordVisible.value)
+                    Icons.Default.Visibility
+                else Icons.Default.VisibilityOff
+
+                // Localized description for accessibility services
+                val description = if (passwordVisible.value) "Hide password" else "Show password"
+
+                // Toggle button to hide or display password
+                IconButton(onClick = { passwordVisible.value = !passwordVisible.value }) {
+                    Icon(imageVector = image, contentDescription = description)
+                }
+            },
             modifier = Modifier
                 .fillMaxWidth()
                 .height(56.dp)
